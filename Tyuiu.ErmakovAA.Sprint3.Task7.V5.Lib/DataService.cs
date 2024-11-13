6915622434b541b25b75094d563f5ada1dc34e30
Alexsandr Ermokov <Tyuiu.ErmakovA.A@outlook.com>
2024-11-14 03:41:51 +0500

    Task7

---
+++ Tyuiu.ErmakovAA.Sprint3.Task7.V5.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.ErmakovAA.Sprint3.Task7.V5.Lib
+{
+    public class DataService : ISprint3Task7V5
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray; // объявляем пустой массив
+            int len = (stopValue - startValue) + 1; // Вычисляем длину массива 
+            valueArray = new double[len]; // Создаем массив с указанной длиной
+            double y; 
+            int count = 0;
+
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = ((2 * Math.Sin(x)) / (3 * x + 1.2)) + Math.Cos(x) - 7 * x * 2;
+                y = Math.Round(y, 2); // Округляем значение до 2 знаков после запятой 
+                valueArray[count] = y; // Присваеваем значение в массив с индексом count
+                count++;
+            }
+            return valueArray;
+           
+        
+        }
+    }
+}
---
+++ Tyuiu.ErmakovAA.Sprint3.Task7.V5/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.ErmakovAA.Sprint3.Task7.V5.Lib;
+namespace Tyuiu.ErmakovAA.Sprint3.Task7.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполгил: Ермаков А.А. | ПКТб-24-1";
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                    *");
+            Console.WriteLine("* Тема: Создание итогового решения по спринту                                  *");
+            Console.WriteLine("* Задание #3                                                                  *");
+            Console.WriteLine("* Вариант #18                                                                  *");
+            Console.WriteLine("* Выполнил: Ермаков А.А.                                                       *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                             *");
+            Console.WriteLine("********************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага первой суммы ряда = " + startValue);
+            Console.WriteLine("Конец шага первой суммы ряда = " + stopValue);
+
+            double[] valueArray = new double[stopValue - startValue + 1];
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                   *");
+            Console.WriteLine("********************************************************************************");
+
+
+            Console.WriteLine("+----------+----------+");
+            Console.WriteLine("|    X     |    f(x)  |");
+            Console.WriteLine("+----------+----------+");
+            for (int i = 0; i <= valueArray.Length - 1; i++)
+            {
+                Console.WriteLine("|{0,5:d}     |  {1, 6:f2}  |", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+----------+----------+");
+            Console.ReadKey();
+        }
+    }
+}
